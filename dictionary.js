841ef2c1557321927a706ecb550dde7d8df36ed5
Icosa <117495728+Icosa-dev@users.noreply.github.com>
2025-10-28 03:54:19 -0400

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,591 @@
+dictionaryDescription = "Alzipar Dictionary",
+dictionaryHaveGlosses = false,
+externalDictionary = [
+    {
+        "word": "Mir",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "First person pronoun",
+                "example": "Kitab mir yamuhifa ",
+                "gloss": "I look (on) human that.",
+                "exampleTranslation": "I have a book"
+            }
+        ]
+    },
+    {
+        "word": "Tu",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Second person pronoun",
+                "example": "Kitab tu yamuhifa ",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "You have a book"
+            }
+        ]
+    },
+    {
+        "word": "Tal",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Third person pronoun, gender neutral",
+                "example": "Kitab tal yamuhifa ",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "They have a book"
+            }
+        ]
+    },
+    {
+        "word": "Mal",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Third person pronoun, masculine",
+                "example": "Kitab mal yamuhifa ",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He has a book"
+            }
+        ]
+    },
+    {
+        "word": "Jal",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Third person pronoun, feminine",
+                "example": "Kitab jal yamuhifa ",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "She has a book"
+            }
+        ]
+    },
+    {
+        "word": "Miri",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "First person pronoun, plural",
+                "example": "Al-Ibar miri šivaca",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "We watered the plant"
+            }
+        ]
+    },
+    {
+        "word": "Tali",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Third person pronoun, plural",
+                "example": "Al-Ibar tali šuvaca",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "They (plural) watered the plant"
+            }
+        ]
+    },
+    {
+        "word": "Hada",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Pronoun for things which are near. Equivalent to \"this\" in English.",
+                "example": "Ibar hada yatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "This is a tree"
+            }
+        ]
+    },
+    {
+        "word": "Kada",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Pronoun for things which are far. Equivalent to \"that\" in English.",
+                "example": "Kitab hada yatubara kai Ibar kada yatubara.",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "This is a book and that is a tree."
+            }
+        ]
+    },
+    {
+        "word": "Kapa",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Used to refer to a thing previously mentioned or easily identified.",
+                "example": "Kitab kapa yatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "It is a book"
+            }
+        ]
+    },
+    {
+        "word": "Kai",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "And",
+                "example": "Mir yakataba kai selemir Ibar mir yašuvatsa",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "I am writing and I am watering my plant"
+            }
+        ]
+    },
+    {
+        "word": "Abai",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "But",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Odai",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "Or",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Vai",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "If",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Epai",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "Because",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Al",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ARTICLE",
+                "explanation": "Defininite article. Used to specify nouns. ",
+                "example": "Al-Ibar kada Ibar yatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "That tree is the tree"
+            }
+        ]
+    },
+    {
+        "word": "Fur",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "For",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Dar",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Towards",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Kon",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "In",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Tep",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "On",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Jor",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "At",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Azu",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "From",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Nyan",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "With",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            }
+        ]
+    },
+    {
+        "word": "Ka",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "Ka",
+                "explanation": "Negation",
+                "example": "Kada mir ka yaduzara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "I don't want that"
+            }
+        ]
+    },
+    {
+        "word": "Hor",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "Adverb",
+                "explanation": "Here",
+                "example": "Hor mir yatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "I am here"
+            }
+        ]
+    },
+    {
+        "word": "Kor",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB",
+                "explanation": "Now",
+                "example": "Kor hor mir yatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "I am now here"
+            }
+        ]
+    },
+    {
+        "word": "Vo",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Who",
+                "example": "Tu vo yastatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Who are you"
+            }
+        ]
+    },
+    {
+        "word": "Var",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "What",
+                "example": "Kada var yastatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "What is that"
+            }
+        ]
+    },
+    {
+        "word": "Vin",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "When",
+                "example": "Tu vin yastazunama",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "When are you comming"
+            }
+        ]
+    },
+    {
+        "word": "Vak",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Where",
+                "example": "Al ibar vac yastatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Where is the tree"
+            }
+        ]
+    },
+    {
+        "word": "Voi",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Why",
+                "example": "Al ibar šivotso voi isktibara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Why is the tree wet"
+            }
+        ]
+    },
+    {
+        "word": "K-T-B",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to writing",
+                "example": "Kitab mal kutaba ",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He wrote a book"
+            },
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "Relates to books",
+                "example": "Kitab mal kutaba",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He wrote a book"
+            },
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB",
+                "explanation": "In a standard way; \"by the books\"",
+                "example": "N/A",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "N/A"
+            },
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADJECTIVE",
+                "explanation": "Intelligent; having the quality of books",
+                "example": "Kitob mal jatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He is smart"
+            }
+        ]
+    },
+    {
+        "word": "T-B-R",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to being",
+                "example": "Kitob mal jatubara",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He is smart"
+            }
+        ]
+    },
+    {
+        "word": "M-H-F",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to having",
+                "example": "Al-Kitab mal jamuhafa",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He has the book"
+            }
+        ]
+    },
+    {
+        "word": "K-F-Z",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to doing",
+                "example": "Tilon tal jakufaza",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "They are doing well"
+            }
+        ]
+    },
+    {
+        "word": "Z-Ts-M",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to going.",
+                "example": "Kada mal jazutsama",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He is going there"
+            }
+        ]
+    },
+    {
+        "word": "Z-N-M",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to coming; arriving",
+                "example": "Mir mal jazunama dar",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He came to me"
+            }
+        ]
+    },
+    {
+        "word": "M-K-M",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to making",
+                "example": "Fikar mal mukama",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He made a fire"
+            }
+        ]
+    },
+    {
+        "word": "R-Z-V",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to recieving",
+                "example": "Al-Kitab mal ruzava",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He got the book"
+            }
+        ]
+    },
+    {
+        "word": "V-B-N",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "Relates to seeing; watching",
+                "example": "Al-Ibar abara mal javubana",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "He is watching the tree grow"
+            }
+        ]
+    }
+]
