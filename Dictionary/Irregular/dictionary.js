0b9d769121adff9f8cb1275a22dca2fefcaafd94
Icosa <117495728+H4drian@users.noreply.github.com>
2025-11-12 22:58:44 -0500

    Updated dictionary

---
+++ Dictionary/Irregular/dictionary.js
@@ -0,0 +1,18 @@
+dictionaryDescription = "Alzapar dictionary for irregular (non-rooted) words",
+dictionaryHaveGlosses = false,
+externalDictionary = [
+    {
+        "word": "Mi",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "First person pronoun",
+                "example": "",
+                "gloss": "I look (on) human that.",
+                "exampleTranslation": ""
+            }
+        ]
+    }
+]
---
+++ Dictionary/Roots/dictionary.js
@@ -0,0 +1,18 @@
+dictionaryDescription = "Alzapar dictionary for roots",
+dictionaryHaveGlosses = false,
+externalDictionary = [
+    {
+        "word": "K-T-B",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "To write",
+                "example": "",
+                "gloss": "I look (on) human that.",
+                "exampleTranslation": ""
+            }
+        ]
+    }
+]
