462e33913f160b916158ee5274cc1190d410c11d
Icosa <117495728+H4drian@users.noreply.github.com>
2025-11-14 16:30:16 -0500

    Updated dictionary

--- dictionary.js
+++ dictionary.js
@@ -1,4 +1,4 @@
-dictionaryDescription = "Alzapar dictionary for roots",
+dictionaryDescription = "Alzapar dictionary. Note that if a word derived from a root breaks any morphological conventions that it will be entered as its own seperate word.",
 dictionaryHaveGlosses = false,
 externalDictionary = [
     {
@@ -9,6 +9,15 @@ externalDictionary = [
                 "badgeText": "",
                 "type": "VERB",
                 "explanation": "To write",
+                "example": "",
+                "gloss": "I look (on) human that.",
+                "exampleTranslation": ""
+            },
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "Book",
                 "example": "",
                 "gloss": "I look (on) human that.",
                 "exampleTranslation": ""
