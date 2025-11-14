3d645d46708ea1f6af7a68fa1e1402657da53f30
Icosa <117495728+H4drian@users.noreply.github.com>
2025-11-14 18:02:23 -0500

    Updated dictionary

--- dictionary.js
+++ dictionary.js
@@ -114,13 +114,274 @@ externalDictionary = [
         ]
     },
     {
-        "word": "K-T-B",
+        "word": "Dan",
         "uses": [
             {
                 "hasBadge": false,
                 "badgeText": "",
-                "type": "VERB",
-                "explanation": "To Write",
+                "type": "PRONOUN",
+                "explanation": "\"This\" or \"it\"",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Dak",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "\"That\"",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "U",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "And",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Imā",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "But",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Yū",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "Or",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Yek",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "If",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Kī",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "CONJUNCTION",
+                "explanation": "Because",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Al",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ARTICLE",
+                "explanation": "Used to make a noun definite",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Gal",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Indication of purpose",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Lī",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Towards",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Fī",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "In or at",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Alay",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Upon",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Min",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "By or via",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            },
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "Who",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Azū",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Hailing from",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Mā",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "With",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "La",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB/ADJECTIVE",
+                "explanation": "Negation or no",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Ħawn",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "Adverb",
+                "explanation": "Here",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Isá",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB",
+                "explanation": "Now",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Máda",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PRONOUN",
+                "explanation": "What",
                 "example": "",
                 "gloss": "help-less-ness",
                 "exampleTranslation": ""
@@ -128,12 +389,180 @@ externalDictionary = [
             {
                 "hasBadge": false,
                 "badgeText": "",
-                "type": "NOUN",
-                "explanation": "Book",
+                "type": "ADVERB",
+                "explanation": "What",
                 "example": "",
                 "gloss": "help-less-ness",
                 "exampleTranslation": ""
             }
         ]
-    }
+    },
+    {
+        "word": "Máta",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB",
+                "explanation": "When",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Fejn",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB",
+                "explanation": "Where",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Limáda",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "ADVERB",
+                "explanation": "Why",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Tu",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Besides or by",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Tayn",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Away",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Fuk",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Over",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Qabil",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Before",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Sab",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Under",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Il",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "(In order) to",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Dwar",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "On the subject of",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Ayr",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Upon",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "Alay",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "PREPOSITION",
+                "explanation": "Upon",
+                "example": "",
+                "gloss": "",
+                "exampleTranslation": ""
+            }
+        ]
+    },
 ]
